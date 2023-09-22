93d5cdabeffcd44ae7f788bba85b6e5944537ec2
Baz Rahimi <bazrahimi@hotmail.com>
2023-09-22 21:41:26 +1000

    add server.js

--- server.js
+++ server.js
@@ -7,3 +7,36 @@ const exphbs = require('express-handlebars');
 // store session
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up session with cookies
+const sess = {
+  secret: 'super secret secret',
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
+});
